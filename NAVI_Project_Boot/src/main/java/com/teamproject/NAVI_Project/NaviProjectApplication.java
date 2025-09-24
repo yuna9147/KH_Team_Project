94b0d0524eebca09ded0c07724364bdc55d939e2
doolgi158 <kolonel@hanmail.net>
2025-09-24 14:53:44 +0900

    초기세팅

---
+++ NAVI_Project_Boot/src/main/java/com/teamproject/NAVI_Project/NaviProjectApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.NAVI_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviProjectApplication.class, args);
+	}
+
+}
---
+++ NAVI_Project_Boot/src/test/java/com/teamproject/NAVI_Project/NaviProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.NAVI_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
